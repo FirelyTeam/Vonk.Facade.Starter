1f4525d65201a40e1ddebf7be584c29c98bc1f14
Corina Ciocanea <corina.ciocanea@gmail.com>
2017-11-16 16:15:11 +0200

    split PatientQueryFactory & BPQueryFactory

---
+++ Vonk.Facade.Starter/Repository/BPQueryFactory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Vonk.Core.Repository;
+using Vonk.Facade.Relational;
+using Vonk.Facade.Starter.Models;
+namespace Vonk.Facade.Starter.Repository
+{
+    public class BloodPressureQuery : RelationalQuery<ViSiBloodPressure>
+    {
+    }
+    public class BPQueryFactory : RelationalQueryFactory<ViSiBloodPressure, BloodPressureQuery>
+    {
+        public BPQueryFactory(DbContext onContext) : base("Observation", onContext) { }
+
+        public override BloodPressureQuery AddValueFilter(string parameterName, TokenValue value)
+        {
+            if (parameterName == "_id")
+            {
+                if (!long.TryParse(value.Code, out long bpId))
+                {
+                    throw new ArgumentException("BloodPressure Id must be an integer value.");
+                }
+                else
+                {
+                    return PredicateQuery(vp => vp.Id == bpId);
+                }
+            }
+            return base.AddValueFilter(parameterName, value);
+        }
+
+        public override BloodPressureQuery AddValueFilter(string parameterName, ReferenceToValue value)
+        {
+            if (parameterName == "subject" && value.Targets.Contains("Patient"))
+            {
+                var patientQuery = value.CreateQuery(new PatientQueryFactory(OnContext));
+                var patIds = patientQuery.Execute(OnContext).Select(p => p.Id);
+
+                return PredicateQuery(bp => patIds.Contains(bp.PatientId));
+            }
+            return base.AddValueFilter(parameterName, value);
+        }
+    }
+}
--- Vonk.Facade.Starter/Repository/PatientQueryFactory.cs
+++ Vonk.Facade.Starter/Repository/PatientQueryFactory.cs
@@ -1,10 +1,6 @@
-﻿using LinqKit;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
 using Vonk.Core.Repository;
 using Vonk.Facade.Relational;
 using Vonk.Facade.Starter.Models;
@@ -12,10 +8,6 @@ using Vonk.Facade.Starter.Models;
 namespace Vonk.Facade.Starter.Repository
 {
     public class PatientQuery : RelationalQuery<ViSiPatient>
-    {
-    }
-
-    public class BloodPressureQuery : RelationalQuery<ViSiBloodPressure>
     {
     }
 
@@ -47,39 +39,6 @@ namespace Vonk.Facade.Starter.Repository
                 var obsIds = obsQuery.Execute(OnContext).Select(bp => bp.PatientId);
 
                 return PredicateQuery(p => obsIds.Contains(p.Id));
-            }
-            return base.AddValueFilter(parameterName, value);
-        }
-    }
-
-    public class BPQueryFactory : RelationalQueryFactory<ViSiBloodPressure, BloodPressureQuery>
-    {
-        public BPQueryFactory(DbContext onContext) : base("Observation", onContext) { }
-
-        public override BloodPressureQuery AddValueFilter(string parameterName, TokenValue value)
-        {
-            if (parameterName == "_id")
-            {
-                if (!long.TryParse(value.Code, out long bpId))
-                {
-                    throw new ArgumentException("BloodPressure Id must be an integer value.");
-                }
-                else
-                {
-                    return PredicateQuery(vp => vp.Id == bpId);
-                }
-            }
-            return base.AddValueFilter(parameterName, value);
-        }
-
-        public override BloodPressureQuery AddValueFilter(string parameterName, ReferenceToValue value)
-        {
-            if (parameterName == "subject" && value.Targets.Contains("Patient"))
-            {
-                var patientQuery = value.CreateQuery(new PatientQueryFactory(OnContext));
-                var patIds = patientQuery.Execute(OnContext).Select(p => p.Id);
-
-                return PredicateQuery(bp => patIds.Contains(bp.PatientId));
             }
             return base.AddValueFilter(parameterName, value);
         }
