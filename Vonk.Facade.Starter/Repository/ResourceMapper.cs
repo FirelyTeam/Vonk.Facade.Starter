7e92509740ca1c822856ac58f5db54fc511442ab
Christiaan Knaap <christiaanfurore@outlook.com>
2018-02-12 14:31:18 +0100

    cosmetic: cleaner object initialization

--- Vonk.Facade.Starter/Repository/ResourceMapper.cs
+++ Vonk.Facade.Starter/Repository/ResourceMapper.cs
@@ -1,10 +1,5 @@
 ﻿using Hl7.Fhir.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Vonk.Core.Common;
-using Vonk.Core.Context;
 using Vonk.Facade.Starter.Models;
 using Vonk.Fhir.R3;
 
@@ -14,11 +9,13 @@ namespace Vonk.Facade.Starter.Repository
     {
         public IResource MapPatient(ViSiPatient source)
         {
-            var patient = new Patient();
-            patient.Id = source.Id.ToString();
+            var patient = new Patient
+            {
+                Id = source.Id.ToString(),
+                BirthDate = new FhirDateTime(source.DateOfBirth).ToString()
+            };
             patient.Identifier.Add(new Identifier("http://mycompany.org/patientnumber", source.PatientNumber));
             patient.Name.Add(new HumanName().WithGiven(source.FirstName).AndFamily(source.FamilyName));
-            patient.BirthDate = new FhirDateTime(source.DateOfBirth).ToString();
             patient.Telecom.Add(new ContactPoint(ContactPoint.ContactPointSystem.Email, ContactPoint.ContactPointUse.Home, source.EmailAddress));
             return patient.AsIResource();
         }
