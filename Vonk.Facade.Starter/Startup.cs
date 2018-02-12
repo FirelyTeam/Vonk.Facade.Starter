c5d9ec53d2378eebb5350d31a435e286bdbc2971
Christiaan Knaap <christiaanfurore@outlook.com>
2018-02-12 14:30:10 +0100

    upgrade: Vonk 0.6.1

--- Vonk.Facade.Starter/Startup.cs
+++ Vonk.Facade.Starter/Startup.cs
@@ -1,33 +1,19 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Vonk.Core.Configuration;
-using Vonk.Fhir.R3;
-using Vonk.Core.Pluggability;
-using Vonk.Core.Operations.Validation;
-using Vonk.Core.Operations.Search;
-using Vonk.Core.Repository;
 using Vonk.Core.Operations.Crud;
+using Vonk.Fhir.R3;
 
 namespace Vonk.Facade.Starter
 {
     public class Startup
     {
-        private readonly IConfigurationRoot _configurationRoot;
-
-        public Startup(IHostingEnvironment env)
-        {
-            _configurationRoot = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile(path: "appsettings.json", reloadOnChange: true, optional: true)
-                .Build();
-        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(_configurationRoot)
+            services
                 .AddFhirServices()
                 .AddVonkMinimalServices()
                 .AddSearchServices()
