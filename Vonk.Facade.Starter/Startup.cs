c882d974f71d1ef8810e58ec8133e69808f52354
Christiaan Knaap <christiaanfurore@outlook.com>
2018-02-12 13:53:16 +0100

    rewrite: Adapt to ASP.NET Core 2 startup style

--- Vonk.Facade.Starter/Startup.cs
+++ Vonk.Facade.Starter/Startup.cs
@@ -1,35 +1,23 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Vonk.Core.Configuration;
-using Vonk.Fhir.R3;
-using Vonk.Core.Pluggability;
-using Vonk.Core.Operations.Validation;
-using Vonk.Core.Operations.Search;
-using Vonk.Core.Repository;
 using Vonk.Core.Operations.Crud;
+using Vonk.Core.Operations.Search;
+using Vonk.Core.Operations.Validation;
+using Vonk.Core.Pluggability;
+using Vonk.Fhir.R3;
 using Vonk.Smart;
 
 namespace Vonk.Facade.Starter
 {
     public class Startup
     {
-        private readonly IConfigurationRoot _configurationRoot;
-
-        public Startup(IHostingEnvironment env)
-        {
-            _configurationRoot = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile(path: "appsettings.json", reloadOnChange: true, optional: true)
-                .Build();
-        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(_configurationRoot)
-                .AddFhirServices()
+            services.AddFhirServices()
                 .AddSmartServices()
                 .AddVonkMinimalServices()
                 .AddSearchServices()
