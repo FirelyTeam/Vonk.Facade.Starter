6a2ec66f66bbaf948492e8c30ffa7fb7f2540a5b
Marco Visser <m.visser@furore.com>
2019-02-05 16:32:58 +0100

    fix: Update Vonk packages and some refactoring

--- Visi.Repository/ResourceMapper.cs
+++ Visi.Repository/ResourceMapper.cs
@@ -3,6 +3,7 @@ using Vonk.Core.Common;
 using Vonk.Core.Context;
 using Visi.Repository.Models;
 using Vonk.Fhir.R3;
+using System;
 
 namespace Visi.Repository
 {
@@ -13,12 +14,12 @@ namespace Visi.Repository
             var patient = new Patient
             {
                 Id = source.Id.ToString(),
-                BirthDate = new FhirDateTime(source.DateOfBirth).ToString()
+                BirthDate = ConvertToFhirDT(source.DateOfBirth).ToString()
             };
             patient.Identifier.Add(new Identifier("http://mycompany.org/patientnumber", source.PatientNumber));
             patient.Name.Add(new HumanName().WithGiven(source.FirstName).AndFamily(source.FamilyName));
             patient.Telecom.Add(new ContactPoint(ContactPoint.ContactPointSystem.Email, ContactPoint.ContactPointUse.Home, source.EmailAddress));
-            return patient.AsIResource();
+            return patient.ToIResource();
         }
 
         public IResource MapBloodPressure(ViSiBloodPressure bp)
@@ -27,7 +28,7 @@ namespace Visi.Repository
             {
                 Id = bp.Id.ToString(),
                 Subject = new ResourceReference($"Patient/{bp.PatientId}"),
-                Effective = new FhirDateTime(bp.MeasuredAt),
+                Effective = ConvertToFhirDT(bp.MeasuredAt),
                 Status = ObservationStatus.Final
             };
             observation.Category.Add(new CodeableConcept("http://hl7.org/fhir/observation-category", "vital-signs", "Vital Signs"));
@@ -44,7 +45,9 @@ namespace Visi.Repository
                     Value = new Quantity(bp.Diastolic, "mm[Hg]", VonkConstants.UcumSystem)
                 });
 
-            return observation.AsIResource();
+            return observation.ToIResource();
         }
+
+        private FhirDateTime ConvertToFhirDT(DateTime dateTime) => new FhirDateTime(new DateTimeOffset(dateTime));
     }
 }
