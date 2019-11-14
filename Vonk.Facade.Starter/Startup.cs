fa31d0c3d044224d76a01aa206fbfa1d0138909d
Christiaan Knaap <christiaanfurore@outlook.com>
2019-11-14 13:05:38 +0100

    upgrade: services extension method has changed in Vonk 3.1.0

--- Vonk.Facade.Starter/Startup.cs
+++ Vonk.Facade.Starter/Startup.cs
@@ -29,7 +29,7 @@ namespace Vonk.Facade.Starter
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddFhirR3Services()
+            services.AddFhirR3FacadeServices(_configuration)
                 .AddSmartServices(_configuration, _hostingEnv)
                 .AddVonkMinimalServices()
                 .AddSearchServices()
